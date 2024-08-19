299441367f0ea9f6abe1b8ba7a13b68989df5e6a
Giovana Raissa <giilimafr@gmail.com>
2024-08-19 07:40:59 -0300

    enviando a primeira versão do projeto

---
+++ src/test/java/br/com/OS/OsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.OS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
