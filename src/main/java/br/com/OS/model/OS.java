f5e17c4f30380700d89ebb93266766a0aab8eed8
Giovana Raissa <giilimafr@gmail.com>
2024-09-17 16:09:16 -0300

    17 setembro

--- src/main/java/br/com/OS/model/OS.java
+++ src/main/java/br/com/OS/model/OS.java
@@ -1,4 +1,25 @@
 package br.com.OS.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class OS {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique=true)
+    private Long id;
+    private String funcionario;
+    private String descricao;
+    private String data;
+    private String ambiente;
+    private String imagem;
+
 }
+
