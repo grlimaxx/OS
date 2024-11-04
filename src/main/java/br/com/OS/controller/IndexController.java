700d703b114003ff7978d929898968e03eb1947c
Giovana Raissa <giilimafr@gmail.com>
2024-11-04 13:52:30 -0300

    Inicio da programação da pagina inicial.

--- src/main/java/br/com/OS/controller/IndexController.java
+++ src/main/java/br/com/OS/controller/IndexController.java
@@ -1,6 +1,9 @@
 package br.com.OS.controller;
 
+import br.com.OS.repository.OsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private OsRepository servicoRepository;
+
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("qtdServicos", servicoRepository.count());
         return "index";
     }
 
